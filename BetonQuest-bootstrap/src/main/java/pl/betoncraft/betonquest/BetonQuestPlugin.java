af8eb514b96214af6deaad62ea198696dadd488c
Brendan Grieve <brendan.grieve@zepli.com.au>
2018-09-21 23:12:17 +0800

    Implement more generic MultiVersion plugin.
    Versions are autodetected now. As long as a Resource of the name MVP/{version} exists in the jar it will load classes under {version} as if they are in the root based upon the server version. It will also look for newer versions in natural order to determine how to load classes.

--- BetonQuest-bootstrap/src/main/java/pl/betoncraft/betonquest/BetonQuestPlugin.java
+++ BetonQuest-bootstrap/src/main/java/pl/betoncraft/betonquest/BetonQuestPlugin.java
@@ -6,14 +6,7 @@ public class BetonQuestPlugin extends MultiVersionPlugin {
 
     public BetonQuestPlugin() {
         super("pl.betoncraft.betonquest",
-                "BetonQuest",
-                new String[] {
-                        "1_8_R3",
-                        "1_9_R2",
-                        "1_10_R1",
-                        "1_11_R1",
-                        "1_12_R1"
-                });
+                "BetonQuest");
     }
 
 }
