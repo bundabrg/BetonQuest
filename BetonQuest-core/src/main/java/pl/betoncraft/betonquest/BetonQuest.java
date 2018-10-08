34e0b6344ba6c8472b7db6ab03330873e110d1f9
Brendan Grieve <brendan.grieve@zepli.com.au>
2018-10-08 11:39:04 +0800

    Multiversion Support for Condition Conversation

--- BetonQuest-core/src/main/java/pl/betoncraft/betonquest/BetonQuest.java
+++ BetonQuest-core/src/main/java/pl/betoncraft/betonquest/BetonQuest.java
@@ -44,6 +44,7 @@ import pl.betoncraft.betonquest.conditions.BiomeCondition;
 import pl.betoncraft.betonquest.conditions.CheckCondition;
 import pl.betoncraft.betonquest.conditions.ChestItemCondition;
 import pl.betoncraft.betonquest.conditions.ConjunctionCondition;
+import pl.betoncraft.betonquest.conditions.ConversationCondition;
 import pl.betoncraft.betonquest.conditions.DayOfWeekCondition;
 import pl.betoncraft.betonquest.conditions.EffectCondition;
 import pl.betoncraft.betonquest.conditions.EmptySlotsCondition;
@@ -236,10 +237,8 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Checks if the condition described by conditionID is met
      *
-     * @param conditionID
-     *            ID of the condition to check, as defined in conditions.yml
-     * @param playerID
-     *            ID of the player which should be checked
+     * @param conditionID ID of the condition to check, as defined in conditions.yml
+     * @param playerID    ID of the player which should be checked
      * @return if the condition is met
      */
     public static boolean condition(String playerID, ConditionID conditionID) {
@@ -287,10 +286,8 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Fires the event described by eventID
      *
-     * @param eventID
-     *            ID of the event to fire, as defined in events.yml
-     * @param playerID
-     *            ID of the player who the event is firing for
+     * @param eventID  ID of the event to fire, as defined in events.yml
+     * @param playerID ID of the player who the event is firing for
      */
     public static void event(String playerID, EventID eventID) {
         // null check
@@ -326,10 +323,8 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Creates new objective for given player
      *
-     * @param playerID
-     *            ID of the player
-     * @param objectiveID
-     *            ID of the objective
+     * @param playerID    ID of the player
+     * @param objectiveID ID of the objective
      */
     public static void newObjective(String playerID, ObjectiveID objectiveID) {
         // null check
@@ -355,12 +350,9 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Resumes the existing objective for given player
      *
-     * @param playerID
-     *            ID of the player
-     * @param objectiveID
-     *            ID of the objective
-     * @param instruction
-     *            data instruction string
+     * @param playerID    ID of the player
+     * @param objectiveID ID of the objective
+     * @param instruction data instruction string
      */
     public static void resumeObjective(String playerID, ObjectiveID objectiveID, String instruction) {
         // null check
@@ -391,13 +383,10 @@ public class BetonQuest extends VersionPlugin {
      * Generates new instance of a Variable. If a similar one was already
      * created, it will return it instead of creating a new one.
      *
-     * @param pack
-     *            package in which the variable is defined
-     * @param instruction
-     *            instruction of the variable, including both % characters.
+     * @param pack        package in which the variable is defined
+     * @param instruction instruction of the variable, including both % characters.
      * @return the Variable instance
-     * @throws InstructionParseException
-     *             when the variable parsing fails
+     * @throws InstructionParseException when the variable parsing fails
      */
     public static Variable createVariable(ConfigPackage pack, String instruction) throws InstructionParseException {
         VariableID ID;
@@ -446,8 +435,7 @@ public class BetonQuest extends VersionPlugin {
      * the user uses the same variables multiple times, the list will contain
      * only one occurence of this variable.
      *
-     * @param text
-     *            text from which the variables will be resolved
+     * @param text text from which the variables will be resolved
      * @return the list of unique variable instructions
      */
     public static ArrayList<String> resolveVariables(String text) {
@@ -585,6 +573,7 @@ public class BetonQuest extends VersionPlugin {
         registerConditions("realtime", RealTimeCondition.class);
         registerConditions("looking", LookingAtCondition.class);
         registerConditions("facing", FacingCondition.class);
+        registerConditions("conversation", ConversationCondition.class);
 
         // register events
         registerEvents("message", MessageEvent.class);
@@ -990,10 +979,8 @@ public class BetonQuest extends VersionPlugin {
      * Stores the PlayerData in a map, so it can be retrieved using
      * getPlayerData(String playerID)
      *
-     * @param playerID
-     *            ID of the player
-     * @param playerData
-     *            PlayerData object to store
+     * @param playerID   ID of the player
+     * @param playerData PlayerData object to store
      */
     public void putPlayerData(String playerID, PlayerData playerData) {
         Debug.info("Inserting data for " + PlayerConverter.getName(playerID));
@@ -1005,8 +992,7 @@ public class BetonQuest extends VersionPlugin {
      * does not exist but the player is online, it will create new playerData on
      * the main thread and put it into the map.
      *
-     * @param playerID
-     *            ID of the player
+     * @param playerID ID of the player
      * @return PlayerData object for the player
      */
     public PlayerData getPlayerData(String playerID) {
@@ -1030,8 +1016,7 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Removes the database playerData from the map
      *
-     * @param playerID
-     *            ID of the player whose playerData is to be removed
+     * @param playerID ID of the player whose playerData is to be removed
      */
     public void removePlayerData(String playerID) {
         playerDataMap.remove(playerID);
@@ -1040,10 +1025,8 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Registers new condition classes by their names
      *
-     * @param name
-     *            name of the condition type
-     * @param conditionClass
-     *            class object for the condition
+     * @param name           name of the condition type
+     * @param conditionClass class object for the condition
      */
     public void registerConditions(String name, Class<? extends Condition> conditionClass) {
         Debug.info("Registering " + name + " condition type");
@@ -1053,10 +1036,8 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Registers new event classes by their names
      *
-     * @param name
-     *            name of the event type
-     * @param eventClass
-     *            class object for the condition
+     * @param name       name of the event type
+     * @param eventClass class object for the condition
      */
     public void registerEvents(String name, Class<? extends QuestEvent> eventClass) {
         Debug.info("Registering " + name + " event type");
@@ -1066,10 +1047,8 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Registers new objective classes by their names
      *
-     * @param name
-     *            name of the objective type
-     * @param objectiveClass
-     *            class object for the objective
+     * @param name           name of the objective type
+     * @param objectiveClass class object for the objective
      */
     public void registerObjectives(String name, Class<? extends Objective> objectiveClass) {
         Debug.info("Registering " + name + " objective type");
@@ -1079,10 +1058,8 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Registers new conversation input/output class.
      *
-     * @param name
-     *            name of the IO type
-     * @param convIOClass
-     *            class object to register
+     * @param name        name of the IO type
+     * @param convIOClass class object to register
      */
     public void registerConversationIO(String name, Class<? extends ConversationIO> convIOClass) {
         Debug.info("Registering " + name + " conversation IO type");
@@ -1092,10 +1069,8 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Registers new variable type.
      *
-     * @param name
-     *            name of the variable type
-     * @param variable
-     *            class object of this type
+     * @param name     name of the variable type
+     * @param variable class object of this type
      */
     public void registerVariable(String name, Class<? extends Variable> variable) {
         Debug.info("Registering " + name + " variable type");
@@ -1105,8 +1080,7 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Returns the list of objectives of this player
      *
-     * @param playerID
-     *            ID of the player
+     * @param playerID ID of the player
      * @return list of this player's active objectives
      */
     public ArrayList<Objective> getPlayerObjectives(String playerID) {
@@ -1120,18 +1094,16 @@ public class BetonQuest extends VersionPlugin {
     }
 
     /**
-     * @param name
-     *            package name, dot and name of the conversation
+     * @param name package name, dot and name of the conversation
      * @return ConversationData object for this conversation or null if it does
-     *         not exist
+     * not exist
      */
     public ConversationData getConversation(String name) {
         return conversations.get(name);
     }
 
     /**
-     * @param objectiveID
-     *            package name, dot and ID of the objective
+     * @param objectiveID package name, dot and ID of the objective
      * @return Objective object or null if it does not exist
      */
     public Objective getObjective(ObjectiveID objectiveID) {
@@ -1153,8 +1125,7 @@ public class BetonQuest extends VersionPlugin {
     }
 
     /**
-     * @param name
-     *            name of the conversation IO type
+     * @param name name of the conversation IO type
      * @return the class object for this conversation IO type
      */
     public Class<? extends ConversationIO> getConvIO(String name) {
@@ -1165,12 +1136,9 @@ public class BetonQuest extends VersionPlugin {
      * Resoles the variable for specified player. If the variable is not loaded
      * yet it will load it on the main thread.
      *
-     * @param packName
-     *            name of the package
-     * @param name
-     *            name of the variable (instruction, with % characters)
-     * @param playerID
-     *            ID of the player
+     * @param packName name of the package
+     * @param name     name of the variable (instruction, with % characters)
+     * @param playerID ID of the player
      * @return the value of this variable for given player
      */
     public String getVariableValue(String packName, String name, String playerID) {
@@ -1203,10 +1171,8 @@ public class BetonQuest extends VersionPlugin {
     /**
      * Renames the objective instance.
      *
-     * @param name
-     *            the current name
-     * @param rename
-     *            the name it should have now
+     * @param name   the current name
+     * @param rename the name it should have now
      */
     public void renameObjective(ObjectiveID name, ObjectiveID rename) {
         objectives.put(rename, objectives.remove(name));
--- BetonQuest-core/src/main/java/pl/betoncraft/betonquest/conditions/ConversationCondition.java
+++ BetonQuest-core/src/main/java/pl/betoncraft/betonquest/conditions/ConversationCondition.java
@@ -1,17 +1,17 @@
-/**
+/*
  * BetonQuest - advanced quests for Bukkit
  * Copyright (C) 2016  Jakub "Co0sh" Sapalski
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -27,34 +27,34 @@ import pl.betoncraft.betonquest.utils.Utils;
 
 /**
  * Checks if the conversation with player has at least one possible option
- *
+ * <p>
  * Example:
- *   {@code conversation <name of conversation>}
+ * {@code conversation <name of conversation>}
  **/
 public class ConversationCondition extends Condition {
 
-	private String conversationID;
+    private String conversationID;
 
-	public ConversationCondition(Instruction instruction) throws InstructionParseException {
-		super(instruction);
+    public ConversationCondition(Instruction instruction) throws InstructionParseException {
+        super(instruction);
 
-		if (instruction.next() == null) {
-			throw new InstructionParseException("Missing conversation parameter");
-		}
+        if (instruction.next() == null) {
+            throw new InstructionParseException("Missing conversation parameter");
+        }
 
-		conversationID = instruction.current();
-	}
+        conversationID = instruction.current();
+    }
 
-	@Override
-	public boolean check(String playerID) throws QuestRuntimeException {
+    @Override
+    public boolean check(String playerID) throws QuestRuntimeException {
 
-		ConversationData conversation = BetonQuest.getInstance().getConversation(Utils.addPackage(instruction.getPackage(), conversationID));
+        ConversationData conversation = BetonQuest.getInstance().getConversation(Utils.addPackage(instruction.getPackage(), conversationID));
 
-		if (conversation == null) {
-			throw new QuestRuntimeException("Conversation does not exist: " + instruction.getPackage().getName() + conversationID);
-		}
+        if (conversation == null) {
+            throw new QuestRuntimeException("Conversation does not exist: " + instruction.getPackage().getName() + conversationID);
+        }
 
-		return conversation.isReady(playerID);
-	}
+        return conversation.isReady(playerID);
+    }
 
 }
