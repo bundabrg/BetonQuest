3e219570ad59a365ed2fbc1b1f9267414b02ce35
Brendan Grieve <brendan.grieve@zepli.com.au>
2018-10-08 12:36:00 +0800

    Multiversion support for JobsReborn

--- BetonQuest-core/src/main/java/pl/betoncraft/betonquest/compatibility/jobsreborn/JobsRebornIntegrator.java
+++ BetonQuest-core/src/main/java/pl/betoncraft/betonquest/compatibility/jobsreborn/JobsRebornIntegrator.java
@@ -1,17 +1,17 @@
-/**
+/*
  * BetonQuest - advanced quests for Bukkit
  * Copyright (C) 2016  Jakub "Co0sh" Sapalski
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -19,24 +19,8 @@ package pl.betoncraft.betonquest.compatibility.jobsreborn;
 
 import org.bukkit.Bukkit;
 import pl.betoncraft.betonquest.BetonQuest;
-import pl.betoncraft.betonquest.compatibility.Compatibility;
 import pl.betoncraft.betonquest.compatibility.Integrator;
-import pl.betoncraft.betonquest.compatibility.citizens.CitizensInventoryConvIO;
-import pl.betoncraft.betonquest.compatibility.citizens.CitizensListener;
-import pl.betoncraft.betonquest.compatibility.citizens.CitizensParticle;
-import pl.betoncraft.betonquest.compatibility.citizens.CitizensVariable;
-import pl.betoncraft.betonquest.compatibility.citizens.CitizensWalkingListener;
-import pl.betoncraft.betonquest.compatibility.citizens.NPCDistanceCondition;
-import pl.betoncraft.betonquest.compatibility.citizens.NPCInteractObjective;
-import pl.betoncraft.betonquest.compatibility.citizens.NPCKillObjective;
-import pl.betoncraft.betonquest.compatibility.citizens.NPCLocationCondition;
-import pl.betoncraft.betonquest.compatibility.citizens.NPCMoveEvent;
-import pl.betoncraft.betonquest.compatibility.citizens.NPCRangeObjective;
-import pl.betoncraft.betonquest.compatibility.citizens.NPCRegionCondition;
-import pl.betoncraft.betonquest.compatibility.protocollib.NPCHider;
-import pl.betoncraft.betonquest.compatibility.protocollib.UpdateVisibilityNowEvent;
 
-import java.util.Arrays;
 import java.util.logging.Level;
 
 
--- BetonQuest-core/src/main/java/pl/betoncraft/betonquest/compatibility/jobsreborn/Objective_PaymentEvent.java
+++ BetonQuest-core/src/main/java/pl/betoncraft/betonquest/compatibility/jobsreborn/Objective_PaymentEvent.java
@@ -1,3 +1,21 @@
+/*
+ * BetonQuest - advanced quests for Bukkit
+ * Copyright (C) 2016  Jakub "Co0sh" Sapalski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package pl.betoncraft.betonquest.compatibility.jobsreborn;
 
 import com.gamingmesh.jobs.api.JobsPaymentEvent;
@@ -11,17 +29,15 @@ import pl.betoncraft.betonquest.InstructionParseException;
 import pl.betoncraft.betonquest.api.Objective;
 import pl.betoncraft.betonquest.utils.PlayerConverter;
 
-public class Objective_PaymentEvent extends Objective implements Listener
-{
+public class Objective_PaymentEvent extends Objective implements Listener {
     private final double nAmount;
-    
-	public Objective_PaymentEvent(Instruction instructions) throws InstructionParseException
-	{
+
+    public Objective_PaymentEvent(Instruction instructions) throws InstructionParseException {
         super(instructions);
         template = ObjectiveData.class;
-		if (instructions.size() < 2) {
-			throw new InstructionParseException("Not enough arguments");
-		}
+        if (instructions.size() < 2) {
+            throw new InstructionParseException("Not enough arguments");
+        }
         try {
             nAmount = Double.parseDouble(instructions.getPart(1));
         } catch (NumberFormatException e) {
@@ -31,25 +47,24 @@ public class Objective_PaymentEvent extends Objective implements Listener
             throw new InstructionParseException("Amount needs to be one or more");
         }
     }
-	
-	@EventHandler
-    public void onJobsPaymentEvent(JobsPaymentEvent event) 
-	{
-		String playerID = PlayerConverter.getID(event.getPlayer().getPlayer().getPlayer());
+
+    @EventHandler
+    public void onJobsPaymentEvent(JobsPaymentEvent event) {
+        String playerID = PlayerConverter.getID(event.getPlayer().getPlayer().getPlayer());
         if (containsPlayer(playerID) && checkConditions(playerID)) {
-        	PaymentData playerData = (PaymentData) dataMap.get(playerID);
-        	Bukkit.getServer().broadcastMessage("Amount: " + playerData.getAmount());
-        	playerData.subtract(event.getAmount());
-        	
-        	if (playerData.isZero()) {
+            PaymentData playerData = (PaymentData) dataMap.get(playerID);
+            Bukkit.getServer().broadcastMessage("Amount: " + playerData.getAmount());
+            playerData.subtract(event.getAmount());
+
+            if (playerData.isZero()) {
                 completeObjective(playerID);
             }
         }
     }
-    
+
     @Override
     public void start() {
-        Bukkit.getPluginManager().registerEvents(this, BetonQuest.getInstance());
+        Bukkit.getPluginManager().registerEvents(this, BetonQuest.getPlugin());
     }
 
     @Override
@@ -61,7 +76,7 @@ public class Objective_PaymentEvent extends Objective implements Listener
     public String getDefaultDataInstruction() {
         return Double.toString(nAmount);
     }
-    
+
     public String getProperty(String name, String playerID) {
         if (name.equalsIgnoreCase("left")) {
             return Double.toString(nAmount - ((PaymentData) dataMap.get(playerID)).getAmount());
@@ -70,25 +85,25 @@ public class Objective_PaymentEvent extends Objective implements Listener
         }
         return "";
     }
-    
+
     public static class PaymentData extends ObjectiveData {
-        
+
         private Double amount;
 
         public PaymentData(String instruction, String playerID, String objID) {
             super(instruction, playerID, objID);
             amount = Double.parseDouble(instruction);
         }
-        
+
         private Double getAmount() {
             return amount;
         }
-        
+
         private void subtract(Double amount) {
-            this.amount -= amount; 
+            this.amount -= amount;
             update();
         }
-        
+
         private boolean isZero() {
             return amount <= 0;
         }
@@ -97,6 +112,6 @@ public class Objective_PaymentEvent extends Objective implements Listener
         public String toString() {
             return Double.toString(amount);
         }
-        
+
     }
 }
