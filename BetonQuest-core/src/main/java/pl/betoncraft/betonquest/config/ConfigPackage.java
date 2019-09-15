f42a12701a004f03a388bed4d0d9c644ff2cb957
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-09-15 23:34:56 +0200

    journal_main_page variables and global variables
    - added support for global variables in journal_main_page
    
    - variables are now available from other packages

--- BetonQuest-core/src/main/java/pl/betoncraft/betonquest/config/ConfigPackage.java
+++ BetonQuest-core/src/main/java/pl/betoncraft/betonquest/config/ConfigPackage.java
@@ -17,6 +17,8 @@
  */
 package pl.betoncraft.betonquest.config;
 
+import pl.betoncraft.betonquest.GlobalVariableID;
+import pl.betoncraft.betonquest.ObjectNotFoundException;
 import pl.betoncraft.betonquest.config.ConfigAccessor.AccessorType;
 import pl.betoncraft.betonquest.utils.Debug;
 import pl.betoncraft.betonquest.utils.Utils;
@@ -165,7 +167,17 @@ public class ConfigPackage {
             Matcher matcher = global_variable_regex.matcher(input);
             if (!matcher.find()) break;
             String varName = matcher.group(1);
-            String varVal = main.getConfig().getString("variables." + varName);
+            String varVal;
+            try
+			{
+				GlobalVariableID variableID = new GlobalVariableID(this, varName);
+				varVal = variableID.getPackage().getMain().getConfig().getString("variables." + variableID.getBaseID());
+			}
+			catch(ObjectNotFoundException e1)
+			{
+                Debug.error(e1.getMessage());
+                return input;
+			}
             if (varVal == null) {
                 Debug.error(String.format("Variable %s not defined in package %s", varName, name));
                 return input;
