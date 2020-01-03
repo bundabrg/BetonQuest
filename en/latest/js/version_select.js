9b2c663e3523e2056f3caa65476076027d26ea60
Travis CI User <travis@example.org>
2020-01-03 04:46:26 +0000

    Deploy Documents: Build 39

--- en/latest/js/version_select.js
+++ en/latest/js/version_select.js
@@ -1,31 +1,7 @@
 window.addEventListener("DOMContentLoaded", function() {
-  function normalizePath(path) {
-    var normalized = [];
-    path.split("/").forEach(function(bit, i) {
-      if (bit === "." || (bit === "" && i !== 0)) {
-        return;
-      } else if (bit === "..") {
-        if (normalized.length === 1 && normalized[0] === "") {
-          // We must be trying to .. past the root!
-          throw new Error("invalid path");
-        } else if (normalized.length === 0 ||
-                   normalized[normalized.length - 1] === "..") {
-          normalized.push("..");
-        } else {
-          normalized.pop();
-        }
-      } else {
-        normalized.push(bit);
-      }
-    });
-    return normalized.join("/");
-  }
-
-  // `base_url` comes from the base.html template for this theme.
-  var REL_BASE_URL = base_url;
-  var ABS_BASE_URL = normalizePath(window.location.pathname + "/" +
-                                   REL_BASE_URL);
-  var CURRENT_VERSION = ABS_BASE_URL.split("/").pop();
+  var BASE_URL = window.location.origin + window.location.path.split("/")[1];
+  var CURRENT_VERSION = window.location.split("/")[3];
+  var CURRENT_LANGUAGE = windows.location.split("/")[2];
 
   function makeSelect(options, selected) {
     var select = document.createElement("select");
@@ -41,7 +17,7 @@ window.addEventListener("DOMContentLoaded", function() {
   }
 
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", REL_BASE_URL + "/../../versions.json");
+  xhr.open("GET", BASE_URL + "/versions.json");
   xhr.onload = function() {
     var versions = JSON.parse(this.responseText);
 
@@ -54,7 +30,7 @@ window.addEventListener("DOMContentLoaded", function() {
       return {text: i.title, value: i.version};
     }), realVersion);
     select.addEventListener("change", function(event) {
-      window.location.href = REL_BASE_URL + "/../" + this.value;
+      window.location.href = BASE_URL + "/" + CURRENT_LANGUAGE + "/" + this.value;
     });
 
     var container = document.createElement("div");
